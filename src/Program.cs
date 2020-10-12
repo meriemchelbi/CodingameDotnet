1faa780e8db14722606e281b913fcadbf5058dff
sprogslegs <meriemchelbi@outlook.com>
2020-10-12 18:32:46 +0100

    ASCII art

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Codingame
 {
@@ -6,7 +8,62 @@ namespace Codingame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int length = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+
+            var letters = new string[height];
+
+            for (int i = 0; i < height; i++)
+            {
+                letters[i] = Console.ReadLine();
+            }
+
+            var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ?";
+            var asciiAlphabet = new Dictionary<char, string[]>();
+
+            var position = 0;
+
+            for (int i = 0; i < 27; i++)
+            {
+                var letter = new string[height];
+
+                for (int row = 0; row < height; row++)
+                {
+                    letter[row] = letters[row].Substring(position, length);
+                    Console.Error.WriteLine($"{letter[row]}");
+                }
+
+                position += length;
+                asciiAlphabet.Add(alphabet[i], letter);
+            }
+
+            var normalisedLetters = input.ToUpperInvariant();
+            var output = new string[height];
+
+            foreach (var letter in normalisedLetters)
+            {
+                string[] asciiLetter;
+
+                var inAlphabet = asciiAlphabet.TryGetValue(letter, out asciiLetter);
+
+                if (!inAlphabet)
+                {
+                    asciiLetter = asciiAlphabet['?'];
+                }
+
+                for (int i = 0; i < height; i++)
+                {
+                    var letterLine = output[i];
+                    output[i] = letterLine + asciiLetter[i];
+                    Console.Error.WriteLine(output[i]);
+                }
+            }
+
+            foreach (var line in output)
+                Console.WriteLine(line);
         }
+
+        
     }
 }
