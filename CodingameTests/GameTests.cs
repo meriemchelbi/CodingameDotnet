7549a3a219fbe6d73a7d8c62a21234e778b24b00
sprogslegs <meriemchelbi@outlook.com>
2020-11-14 12:39:46 +0000

    Added Recipe helpers for tests

--- CodingameTests/GameTests.cs
+++ CodingameTests/GameTests.cs
@@ -21,15 +21,15 @@ namespace CodingameTests
         public GameTests()
         {
             _sut = new Game();
-            _brew47 = MakeBrewRecipe(47, -3, -2, 0, 0);
-            _brew49 = MakeBrewRecipe(49, 0, 0, -5, 0);
-            _brew50 = MakeBrewRecipe(50, -2, 0, 0, -2);
-            _brew54 = MakeBrewRecipe(54, 0, 0, -2, -2);
-            _brew65 = MakeBrewRecipe(65, 0, 0, 0, -5);
-            _cast78 = MakeCastRecipe(78, 2, 0, 0, 0);
-            _cast79 = MakeCastRecipe(79, -1, 1, 0, 0);
-            _cast80 = MakeCastRecipe(80, 0, -1, 1, 0);
-            _cast82 = MakeCastRecipe(82, 2, 0, 0, 0);
+            _brew47 = RecipeHelpers.MakeBrewRecipe(47, -3, -2, 0, 0);
+            _brew49 = RecipeHelpers.MakeBrewRecipe(49, 0, 0, -5, 0);
+            _brew50 = RecipeHelpers.MakeBrewRecipe(50, -2, 0, 0, -2);
+            _brew54 = RecipeHelpers.MakeBrewRecipe(54, 0, 0, -2, -2);
+            _brew65 = RecipeHelpers.MakeBrewRecipe(65, 0, 0, 0, -5);
+            _cast78 = RecipeHelpers.MakeCastRecipe(78, 2, 0, 0, 0);
+            _cast79 = RecipeHelpers.MakeCastRecipe(79, -1, 1, 0, 0);
+            _cast80 = RecipeHelpers.MakeCastRecipe(80, 0, -1, 1, 0);
+            _cast82 = RecipeHelpers.MakeCastRecipe(82, 2, 0, 0, 0);
         }
 
 
@@ -105,42 +105,6 @@ namespace CodingameTests
             var result = _sut.DecideAction();
 
             result.Should().Be("REST");
-        }
-        
-        [Fact]
-        public void DecideAction_Custom()
-        {
-            _cast82.IsCastable = true;
-
-            _sut.Me.Inventory = new List<int> { 0, 0, 0, 0 };
-            _sut.Recipes = new List<Recipe> { _cast82 };
-
-            var result = _sut.DecideAction();
-
-            result.Should().Be("REST");
-        }
-
-
-        private Recipe MakeBrewRecipe(int id, params int[] ingredients)
-        {
-            return new Recipe
-            {
-                Id = id,
-                IsCastable = false,
-                Ingredients = ingredients,
-                Type = ActionType.BREW
-            };
-        }
-        
-        private Recipe MakeCastRecipe(int id, params int[] ingredients)
-        {
-            return new Recipe
-            {
-                Id = id,
-                IsCastable = false,
-                Ingredients = ingredients,
-                Type = ActionType.CAST
-            };
         }
     }
 }
---
+++ CodingameTests/RecipeHelpers.cs
@@ -0,0 +1,29 @@
+﻿using Codingame;
+
+namespace CodingameTests
+{
+    public static class RecipeHelpers
+    {
+        public static Recipe MakeBrewRecipe(int id, params int[] ingredients)
+        {
+            return new Recipe
+            {
+                Id = id,
+                IsCastable = false,
+                Ingredients = ingredients,
+                Type = ActionType.BREW
+            };
+        }
+
+        public static Recipe MakeCastRecipe(int id, params int[] ingredients)
+        {
+            return new Recipe
+            {
+                Id = id,
+                IsCastable = false,
+                Ingredients = ingredients,
+                Type = ActionType.CAST
+            };
+        }
+    }
+}
