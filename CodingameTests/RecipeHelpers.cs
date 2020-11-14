62e69bb3def6454c7a513c730e9ea5d8619b1762
sprogslegs <meriemchelbi@outlook.com>
2020-11-14 10:05:26 +0000

    Added recipe helper methods for tests

---
+++ CodingameTests/RecipeHelpers.cs
@@ -0,0 +1,30 @@
+﻿using Codingame;
+
+namespace CodingameTests
+{
+    public static class RecipeHelpers
+    {
+        public static Recipe MakeBrewRecipe(int id, int price, params int[] ingredients)
+        {
+            return new Recipe
+            {
+                Id = id,
+                Price = price,
+                IsCastable = false,
+                Ingredients = ingredients,
+                Type = ActionType.BREW
+            };
+        }
+
+        public static Recipe MakeCastRecipe(int id, params int[] ingredients)
+        {
+            return new Recipe
+            {
+                Id = id,
+                IsCastable = false,
+                Ingredients = ingredients,
+                Type = ActionType.CAST
+            };
+        }
+    }
+}
